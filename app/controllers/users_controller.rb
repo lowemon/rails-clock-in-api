45f99b286d6746199f65c93e61d778aa429723fd
Louie Escano <louie@gorated.ph>
2023-04-03 06:05:59 +0800

    Added users controller index for testing

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
   # Test -- Retrieve all Users and ID will remove
   def index
+    @users = User.all
+    render json: @users
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :users, only: %i[index] do
     resources :clock_ins, only: %i[index clock_in clock_out]
-    resources :follows, only: %i[index follow_user unfollow_user]
+    resources :follows, only: %i[followed_records follow_user unfollow_user]
   end
 end
