a973f94a4782b1a3f8049dd49cc05a575265000b
Louie Escano <louie@gorated.ph>
2023-04-03 04:13:13 +0800

    Add DB columns for each model

--- db/migrate/20230402190053_create_clock_ins.rb
+++ db/migrate/20230402190053_create_clock_ins.rb
@@ -1,6 +1,8 @@
 class CreateClockIns < ActiveRecord::Migration[6.1]
   def change
     create_table :clock_ins do |t|
+      t.datetime :clocked_in_at
+      t.references :user, foreign_key: true
 
       t.timestamps
     end
---
+++ db/schema.rb
@@ -0,0 +1,51 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 2023_04_02_190447) do
+
+  create_table "clock_ins", force: :cascade do |t|
+    t.datetime "clocked_in_at"
+    t.integer "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_clock_ins_on_user_id"
+  end
+
+  create_table "follows", force: :cascade do |t|
+    t.integer "follower_id", null: false
+    t.integer "followed_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["followed_id"], name: "index_follows_on_followed_id"
+    t.index ["follower_id"], name: "index_follows_on_follower_id"
+  end
+
+  create_table "sleep_records", force: :cascade do |t|
+    t.integer "user_id"
+    t.float "length"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_sleep_records_on_user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "email", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  add_foreign_key "clock_ins", "users"
+  add_foreign_key "follows", "followeds"
+  add_foreign_key "follows", "followers"
+  add_foreign_key "sleep_records", "users"
+end
